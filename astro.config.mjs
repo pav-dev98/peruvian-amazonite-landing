a1a5db9203392692bf79be016f12e5bc47f6b21f
pavel mansilla <pavel.mansilla@gmail.com>
2026-01-28 02:15:30 -0300

    fix(deploy): set output to 'server' with vercel adapter for astro actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,15 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
-	},
+    },
 
   integrations: [react()],
+  output: 'server',
+  adapter: vercel(),
 });
