fadc1d056d3f4ca79a6d0632a403fccb833569e7
pavel mansilla <pavel.mansilla@gmail.com>
2026-03-05 21:00:39 -0300

    feat(ui): update company name in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -50,7 +50,7 @@ const Header = () => {
             <div>
               <span className={`font-serif font-bold text-lg transition-colors ${isScrolled ? "text-foreground" : "text-primary-foreground"
                 }`}>
-                Blue Peruvian Amazonite.
+                Blue Amazonite Peru.
               </span>
             </div>
           </a>
